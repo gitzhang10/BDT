4004e31b27d6d00302bffae52f58fc7c89d56756
gitzhang10 <93389761+gitzhang10@users.noreply.github.com>
2022-09-19 17:41:29 +0800

    Add files via upload

--- myexperiements/sockettest/hbbft_node.py
+++ myexperiements/sockettest/hbbft_node.py
@@ -2,11 +2,12 @@ import random
 import gevent
 import os
 import pickle
-
+from typing import  Callable
 from gevent import time
 from honeybadgerbft.core.honeybadger import HoneyBadgerBFT
 from myexperiements.sockettest.make_random_tx import tx_generator
 from multiprocessing import Value as mpValue, Queue as mpQueue, Process
+from ctypes import c_bool
 
 
 def load_key(id):
@@ -28,14 +29,20 @@ def load_key(id):
 
 class HoneyBadgerBFTNode (HoneyBadgerBFT):
 
-    def __init__(self, sid, id, B, N, f, my_address: str, addresses_list: list, K=3, mode='debug', mute=False, tx_buffer=None):
+    def __init__(self, sid, id, B, N, f, bft_from_server: Callable, bft_to_client: Callable, ready: mpValue, stop: mpValue, K=3, mode='debug', mute=False, bft_running: mpValue=mpValue(c_bool, False), tx_buffer=None):
         self.sPK, self.ePK, self.sSK, self.eSK = load_key(id)
-        HoneyBadgerBFT.__init__(self, sid, id, B, N, f, self.sPK, self.sSK, self.ePK, self.eSK, send=None, recv=None, K=K, mute=mute)
-        self.server = Node(id=id, ip=my_address, port=addresses_list[id][1], addresses_list=addresses_list, logger=self.logger)
+        self.bft_from_server = bft_from_server
+        self.bft_to_client = bft_to_client
+        self.send = lambda j, o: self.bft_to_client((j, o))
+        self.recv = lambda: self.bft_from_server()
+        self.ready = ready
+        self.stop = stop
         self.mode = mode
-        self._prepare_bootstrap()
+        self.running = bft_running
+        HoneyBadgerBFT.__init__(self, sid, id, B, N, f, self.sPK, self.sSK, self.ePK, self.eSK, self.send, self.recv, K=K, mute=mute)
 
-    def _prepare_bootstrap(self):
+    def prepare_bootstrap(self):
+        self.logger.info('node id %d is inserting dummy payload TXs' % (self.id))
         if self.mode == 'test' or 'debug':
             for r in range(self.K * self.B):
                 tx = tx_generator(250) # Set each dummy TX to be 250 Byte
@@ -43,34 +50,25 @@ class HoneyBadgerBFTNode (HoneyBadgerBFT):
         else:
             pass
             # TODO: submit transactions through tx_buffer
+        self.logger.info('node id %d completed the loading of dummy TXs' % (self.id))
 
-    def start_socket_server(self):
+    def run(self):
+
         pid = os.getpid()
-        #print('pid: ', pid)
-        self.logger.info('node id %d is running on pid %d' % (self.id, pid))
-        self.server.start()
+        self.logger.info('node %d\'s starts to run consensus on process id %d' % (self.id, pid))
 
-    def connect_socket_servers(self):
-        self.server.connect_and_send_forever()
-        self._send = self.server.send
-        self._recv = self.server.recv
+        self.prepare_bootstrap()
 
-    def run_hbbft_instance(self):
-        self.start_socket_server()
-        time.sleep(3)
-        gevent.sleep(3)
-        self.connect_socket_servers()
-        time.sleep(3)
-        gevent.sleep(3)
-        self.run()
-        time.sleep(3)
-        gevent.sleep(3)
-        self.server.stop_service()
+        while not self.ready.value:
+            time.sleep(1)
 
+        self.running.value = True
+        self.run_bft()
+        self.stop.value = True
 
 def main(sid, i, B, N, f, addresses, K):
     badger = HoneyBadgerBFTNode(sid, i, B, N, f, addresses, K)
-    badger.run_hbbft_instance()
+    badger.run_bft()
 
 
 if __name__ == '__main__':
