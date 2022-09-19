c76ef452ef3d4fd1ce50d7d95b7ba792e35546be
gitzhang10 <93389761+gitzhang10@users.noreply.github.com>
2022-09-19 15:53:16 +0800

    Add files via upload

--- run_local_network_test.sh
+++ run_local_network_test.sh
@@ -5,9 +5,10 @@ echo "start.sh <N> <F> <B> <K>"
 
 python3 run_trusted_key_gen.py --N $1 --f $2
 
-llall python3
+killall python3
 i=0
 while [ "$i" -lt $1 ]; do
     echo "start node $i..."
-    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 50 --T 1 --F 1000000 & i=$(( i + 1 ))
+    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 50 --T 1 --F 1000000 --P "dumbo" &
+    i=$(( i + 1 ))
 done 
