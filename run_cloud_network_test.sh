da9a6287858cb05e3dee413c682d608cb6197537
gitzhang10 <93389761+gitzhang10@users.noreply.github.com>
2022-09-21 16:04:28 +0800

    Update run_cloud_network_test.sh

--- run_cloud_network_test.sh
+++ run_cloud_network_test.sh
@@ -4,16 +4,16 @@
 N=4
 
 # public IPs --- This is the public IPs of AWS servers
- pubIPsVar=([0]='192.168.1.2'
- [1]='192.168.1.3'
- [2]='192.168.1.4'
- [3]='192.168.1.5')
+ pubIPsVar[0]='192.168.1.2'
+ pubIPsVar[1]='192.168.1.3'
+ pubIPsVar[2]='192.168.1.4'
+ pubIPsVar[3]='192.168.1.5'
  
 # private IPs --- This is the private IPs of AWS servers
- priIPsVar=([0]='192.168.1.2'
- [1]='192.168.1.3'
- [2]='192.168.1.4'
- [3]='192.168.1.5')
+ priIPsVar[0]='192.168.1.2'
+ priIPsVar[1]='192.168.1.3'
+ priIPsVar[2]='192.168.1.4'
+ priIPsVar[3]='192.168.1.5'
 
 # Clone code to all remote AWS servers from github
  i=0; while [ $i -le $(( N-1 )) ]; do
