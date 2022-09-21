d581f619dace232b6cec08ec1f9ad9c94002b0ef
gitzhang10 <93389761+gitzhang10@users.noreply.github.com>
2022-09-21 15:43:08 +0800

    Update run_cloud_network_test.sh

--- run_cloud_network_test.sh
+++ run_cloud_network_test.sh
@@ -17,7 +17,7 @@ N = 4
 
 # Clone code to all remote AWS servers from github
  i=0; while [ $i -le $(( N-1 )) ]; do
- ssh -i "/home/vagrant/.ssh/dumbo.pem" -o StrictHostKeyChecking=no ubuntu@${pubIPsVar[i]} "git clone https://github.com/gitzhang10/BDT.git" &
+ ssh -i "/home/vagrant/.ssh/dumbo.pem" -o StrictHostKeyChecking=no vagrant@${pubIPsVar[i]} "git clone https://github.com/gitzhang10/BDT.git" &
  i=$(( i+1 ))
  done
 
@@ -28,18 +28,18 @@ N = 4
    i=$(( i+1 ))
  done
  i=0; while [ $i -le $(( N-1 )) ]; do
-   ssh -o "StrictHostKeyChecking no" -i "/home/vagrant/.ssh/dumbo.pem" ubuntu@${pubIPsVar[i]} "rm /home/vagrant/BDT/hosts.config"
-   scp -i "/home/vagrant/.ssh/dumbo.pem" tmp_hosts.config ubuntu@${pubIPsVar[i]}:/home/vagrant/BDT/hosts.config &
+   ssh -o "StrictHostKeyChecking no" -i "/home/vagrant/.ssh/dumbo.pem" vagrant@${pubIPsVar[i]} "rm /home/vagrant/BDT/hosts.config"
+   scp -i "/home/vagrant/.ssh/dumbo.pem" tmp_hosts.config vagrant@${pubIPsVar[i]}:/home/vagrant/BDT/hosts.config &
    i=$(( i+1 ))
  done
  
  # Start Protocols at all remote AWS servers
- i=0; while [ $i -le $(( N-1 )) ]; do   ssh -i "/home/vagrant/.ssh/dumbo.pem" ubuntu@${pubIPsVar[i]} "export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; cd BDT; nohup python3 run_socket_node.py --sid 'sidA' --id $i --N $N --f $(( (N-1)/3 )) --B 1000 --K 20 --S 50 --T 2 --P "dumbo" --F 1000000 > node-$i.out" &   i=$(( i+1 )); done
+ i=0; while [ $i -le $(( N-1 )) ]; do   ssh -i "/home/vagrant/.ssh/dumbo.pem" vagrant@${pubIPsVar[i]} "export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; cd BDT; nohup python3 run_socket_node.py --sid 'sidA' --id $i --N $N --f $(( (N-1)/3 )) --B 1000 --K 20 --S 50 --T 2 --P "dumbo" --F 1000000 > node-$i.out" &   i=$(( i+1 )); done
 
  # Download logs from all remote AWS servers to your local PC
  i=0
  while [ $i -le $(( N-1 )) ]
  do
-   scp -i "/home/vagrant/.ssh/dumbo.pem" ubuntu@${pubIPsVar[i]}:/home/vagrant/BDT/log/node-$i.log node-$i.log &
+   scp -i "/home/vagrant/.ssh/dumbo.pem" vagrant@${pubIPsVar[i]}:/home/vagrant/BDT/log/node-$i.log node-$i.log &
    i=$(( i+1 ))
  done
