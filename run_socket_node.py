e7f22fccce017abed581fecd004d70534fe67c00
gitzhang10 <93389761+gitzhang10@users.noreply.github.com>
2022-09-23 15:56:33 +0800

    Update run_socket_node.py

--- run_socket_node.py
+++ run_socket_node.py
@@ -141,10 +141,8 @@ if __name__ == '__main__':
             net_ready.value = True
 
         bft_thread = Greenlet(bft.run)
-        print("start")
         bft_thread.start()
         bft_thread.join()
-        print("over")
 
         with stop.get_lock():
             stop.value = True
